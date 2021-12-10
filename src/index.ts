cb0e44d01a4f62f68fe80a1df96db24db07bb3eb
kauefraga <kauefragarodrigues456@gmail.com>
2021-12-10 16:57:56 -0300

    feat: route '/'

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import express from 'express'
 
+import routes from './routes'
+
 const app = express()
+
+app.use(routes)
+
+
+app.listen(3333, () => {
+  console.log('[server] Running on http://localhost:3333')
+})
