feed626098c842a004daa1a82b9ee844445a775b
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-01-29 16:41:29 -0300

    :sparkles: feat: add '/register' POST route

--- src/routes.ts
+++ src/routes.ts
@@ -1,31 +1,53 @@
+import 'dotenv/config'
 import { Router } from 'express'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const routes = Router()
 
 const prisma = new PrismaClient()
 
-routes.get('/', (req, res) => { res.json({ message: "Hello world!" }) })
+routes.get('/', (req, res) => { res.json({ message: 'Hello world!' }) })
 
-routes.get('/getNewsletter', async ({ params }, response) => {
-  const allPosts = await prisma.post.findMany({})
-  // use `console.dir` to print nested objects
-  console.dir(allPosts, { depth: null })
-})
+routes.post('/register', async (request, response) => {
+  const { name, email, password }: User = request.body
 
-routes.post('/saveNewsletter', async (request, response) => {
-  const { subject, body } = request.body
+  if (!(name && email && password)) {
+    return response.status(400).json({
+      message: 'Please enter a name, email and password.'
+    })
+  }
 
-  if (subject && body) return response.status(400)
-
-  const newPost = await prisma.post.create({
-    data: { subject, body }
+  const isUserAlreadyRegistered = await prisma.user.findUnique({
+    where: { email }
   })
 
-  return response.status(200).json({
-    message: 'Now, you can find your post searching by id/subject',
-    id: newPost.id,
-    subject
+  if (isUserAlreadyRegistered) {
+    return response.status(409).json('User Already Exist. Please Login.')
+  }
+
+  const encryptedPassword = await bcrypt.hash(password, 10)
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email: email.toLowerCase(),
+      password: encryptedPassword
+    }
+  })
+
+  const token = jwt.sign(
+    { user_id: user.id, email },
+    process.env.TOKEN_KEY,
+    { expiresIn: "2h" }
+  )
+
+  user.token = token
+
+  return response.status(201).json({
+    message: 'User successfully created',
+    user
   })
 })
 
