42c11a7f57dd7f6616704a418a8ca53c4afdabc2
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-01-29 16:40:31 -0300

    :sparkles: feat: add cors and json parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
 import routes from './routes'
 
 const app = express()
 
-const prisma = new PrismaClient()
-
+app.use(cors())
+app.use(express.json())
 app.use(routes)
 
 app.listen(3333, () => {
