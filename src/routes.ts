7f5c165b6e7b5ada0c10892340f9ef3960962516
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2022-01-29 19:04:26 -0300

    :sparkles: feat: add ´/login´ POST and ´/welcome´ GET+middleware routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,22 @@
-import 'dotenv/config'
 import { Router } from 'express'
 import { PrismaClient, User } from '@prisma/client'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import auth from './middleware/auth'
 
 const routes = Router()
 
 const prisma = new PrismaClient()
 
-routes.get('/', (req, res) => { res.json({ message: 'Hello world!' }) })
+
+routes.get('/', (req, res) => { res.json('Hello world!') })
 
 routes.post('/register', async (request, response) => {
   const { name, email, password }: User = request.body
 
   if (!(name && email && password)) {
     return response.status(400).json({
-      message: 'Please enter a name, email and password.'
+      error: 'Please enter a name, email and password.'
     })
   }
 
@@ -24,7 +25,9 @@ routes.post('/register', async (request, response) => {
   })
 
   if (isUserAlreadyRegistered) {
-    return response.status(409).json('User Already Exist. Please Login.')
+    return response.status(409).json({
+      error: 'User Already Exist. Please Login.'
+    })
   }
 
   const encryptedPassword = await bcrypt.hash(password, 10)
@@ -40,14 +43,44 @@ routes.post('/register', async (request, response) => {
   const token = jwt.sign(
     { user_id: user.id, email },
     process.env.TOKEN_KEY,
-    { expiresIn: "2h" }
+    { expiresIn: 60 * 60 * 12 } // 12 hours
   )
 
   user.token = token
 
-  return response.status(201).json({
-    message: 'User successfully created',
-    user
+  return response.status(201).json(user)
+})
+
+routes.post('/login', async (request, response) => {
+  const { email, password }: User = request.body
+
+  if (!(email && password)) {
+    response.status(400).json({ error: 'Please enter a email and password.' })
+  }
+
+  const user: User | any = await prisma.user.findUnique({ where: { email } })
+
+  const validatePassword = await bcrypt.compare(password, user.password)
+
+  if (user && validatePassword) {
+    const token = jwt.sign(
+      { user_id: user.id, email },
+      process.env.TOKEN_KEY,
+      { expiresIn: 60 * 60 * 12 } // 12 hours
+    )
+
+    user.token = token
+
+    response.status(200).json(user)
+  }
+
+  response.status(400).json({ error: 'Invalid Credentials' })
+})
+
+routes.get('/welcome', auth, (request, response) => {
+  return response.json({
+    message: '🔥 Welcome! Congrats to sign up! 🔥',
+    isAuthenticated: true
   })
 })
 
