1ad0dae8ed4e2dbd58414773b09eedebcd40d13d
kauefraga <kauefragarodrigues456@gmail.com>
2022-06-09 21:23:07 -0300

    test: set up e2e env

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+export default {
+  clearMocks: true,
+
+  // collectCoverage: true,
+  // coverageDirectory: "__tests__/coverage",
+  // An array of regexp pattern strings used to skip coverage collection
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+  // coverageProvider: "v8",
+  // coverageReporters: [
+  //   "json",
+  //   "lcov"
+  // ],
+
+  preset: 'ts-jest',
+
+  testMatch: [
+    '**/?(*.)+(spec|test).ts',
+  ],
+
+};
