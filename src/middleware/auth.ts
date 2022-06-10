14e6ac05566e2df6450e8df0421e561b532f60cf
kauefraga <kauefragarodrigues456@gmail.com>
2022-06-09 21:19:56 -0300

    lint: just linted with airbnb

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,27 +1,28 @@
-import { Request, Response, NextFunction } from 'express'
-import jwt from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
-function verifyToken (request: Request, response: Response, next: NextFunction) {
+function verifyToken(request: Request, response: Response, next: NextFunction) {
   const token: string = request.body.token
   || request.query.token
-  || request.headers['authorization']
+  || request.headers.authorization;
 
   if (!token) {
     return response.status(403).json({
-      error: 'A token is required for authentication'
-    })
+      error: 'A token is required for authentication',
+    });
   }
 
-  const [bearer, jwtToken] = token.split(' ')
+  // eslint-disable-next-line no-unused-vars
+  const [bearer, jwtToken] = token.split(' ');
 
   try {
-    const decodedToken = jwt.verify(jwtToken, process.env.TOKEN_KEY)
-    request.signedCookies = decodedToken
+    const decodedToken = jwt.verify(jwtToken, process.env.TOKEN_KEY);
+    request.signedCookies = decodedToken;
   } catch (error) {
-    return response.status(401).json({ error: 'Invalid token' })
+    return response.status(401).json({ error: 'Invalid token' });
   }
 
-  return next()
+  return next();
 }
 
-export default verifyToken
+export default verifyToken;
